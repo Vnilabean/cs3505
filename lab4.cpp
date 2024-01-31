95e3b8e7f871580053171a87f07e247c7fe3d216
Phil <95306018+Vnilabean@users.noreply.github.com>
2024-01-31 16:29:44 -0700

    Create lab4.cpp

---
+++ lab4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+int* getIntPtr() {
+    int number = 5;
+    int* ptr = &number;
+    return ptr;
+}
+
+
+
+
+int main() {
+  int* val = new int[4];
+  int* same_mem = val;
+
+  std::cout << val << " " << same_mem << std::endl;
+  delete [] val;
+
+  int* ptr = getIntPtr();
+    std::cout << *ptr << std::endl;
+}

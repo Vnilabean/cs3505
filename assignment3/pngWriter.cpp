7a6ec8b7e9f5e3ee67a197bcce1bc7f32bd2cc33
Phil <95306018+Vnilabean@users.noreply.github.com>
2024-01-31 23:30:33 -0700

    update pngWriter
    changed api to add a color selector for set pixel

--- assignment3/pngWriter.cpp
+++ assignment3/pngWriter.cpp
@@ -1,6 +1,7 @@
 /**
  * @author Philippe Gonzalez
  * @note CS3505 Assignment 3
+ * @note Most of the code in this file is from the examplePNG code, just formatted to work for the assignment
 */
 
 #include "pngWriter.h"
@@ -14,6 +15,9 @@ png_bytep* imageRows;
 
 PNGWriter::PNGWriter() {}
 
+/**
+ * @brief Constructs a PNGWriter object with the specified width and height
+*/
 PNGWriter::PNGWriter(int width, int height) {
     this->width = width;
     this->height = height;
@@ -22,11 +26,17 @@ PNGWriter::PNGWriter(int width, int height) {
     imageRows = new png_bytep[height] {};
 }
 
+/**
+ * @brief Destroys the PNGWriter object
+*/
 PNGWriter::~PNGWriter() {
     delete[] imageData;
     delete[] imageRows;
 }
 
+/**
+ * @brief Writes the image data to a PNG file with the specified filename
+*/
 void PNGWriter::writePNGFile(char *filename) {
     FILE *fp = fopen(filename, "wb");
     if(!fp) abort();
@@ -67,14 +77,24 @@ void PNGWriter::writePNGFile(char *filename) {
     png_destroy_write_struct(&png, &info);
 }
 
-void PNGWriter::setPixel(int x, int y) {
+ /**
+  * @brief Sets the pixel at the specified coordinates
+ */
+void PNGWriter::setPixel(int x, int y, int red, int green, int blue, int alpha) {
     // offset used from the exmaple
     int offset = (y * width + x) * 4;
 
-    // Set the pixel to fern color
-    imageData[offset] = 104;       // red
-    imageData[offset + 1] = 160;   // green
-    imageData[offset + 2] = 100;   // blue
-    imageData[offset + 3] = 255; // alpha)
+    // Set the pixel to specified color
+    imageData[offset] = red;       
+    imageData[offset + 1] = green;   
+    imageData[offset + 2] = blue;   
+    imageData[offset + 3] = alpha; 
     
+}
+
+/**
+ * @brief Sets the pixel at the specified coordinates to black
+*/
+void PNGWriter::setPixel(int x, int y) {
+    setPixel(x, y, 0, 0, 0, 255);
 }

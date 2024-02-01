d23d48af8e26019ccd73f2077e98b2f119a0c915
Phil <95306018+Vnilabean@users.noreply.github.com>
2024-01-31 19:16:25 -0700

    Added point library

---
+++ assignment3/point.cpp
@@ -0,0 +1,45 @@
+#include "point.h"
+#include <iostream>
+/*
+Point class for CS 3505 lecture 3.
+A Point holds x and y values.
+By Eric Heisler and STUDENT_NAME
+*/
+
+Point::Point() {}
+
+Point::Point(double initx, double inity) : x(initx), y(inity) {}
+
+Point::~Point() {}
+
+double Point::getX() {
+    return x;
+}
+
+double Point::getY() {
+    return y;
+}
+
+// Operators
+Point Point::operator*(double scaleFactor) {
+	Point res(x*scaleFactor, y*scaleFactor);
+	return res;
+}
+
+// This doesn't start with Point:: so it is not class code.
+std::ostream& operator<<(std::ostream& output, Point pt) {
+    output << pt.x << ", " << pt.y;
+    return output;
+}
+
+// Implementation the friend operation for *=
+Point& operator*=(Point& pt, Transform tr) {
+    // Get the values for the point
+    double x = pt.getX();
+    double y = pt.getY();
+    // apply them to the transformation values
+    double newX = tr.transformValues[0] * x + tr.transformValues[1] * y + tr.transformValues[2];
+    double newY = tr.transformValues[3] * x + tr.transformValues[4] * y + tr.transformValues[5];
+    pt = Point(newX, newY);
+    return pt;
+}
---
+++ assignment3/point.h
@@ -0,0 +1,84 @@
+#ifndef POINT_H
+#define POINT_H
+/*
+Point class for CS 3505 lecture 3.
+A Point holds x and y values.
+By Eric Heisler and Philippe Gonzalez
+*/
+
+#include <iostream> // for ostream
+#include "transform.h"
+class Transform;
+
+/**
+ * @brief A Point class that represents a point in 2D
+ * This class provides a way to store and manipulate the coordinates of a 2D point
+ */
+class Point {
+private:
+    double x, y;  // The x and y coordinates of the point
+
+public:
+
+    /**
+     * @brief Default constructor that initializes the point to 0, 0
+     */
+    Point();
+
+    /**
+     * @brief The constructor that initializes the point with the given x, y coordinates
+     * 
+     * @param initX initial x coordinate of the point
+     * @param initY initial y coordinate of the point
+     */
+    Point(double initX, double initY);
+
+    /**
+     * @brief Destructor for the Point class
+     * 
+     * The  destructor does nothing in this class
+     */
+    ~Point();
+
+    /**
+     * @brief Getter for the x coordinate of the point
+     * 
+     * @return The x coordinate of the point
+     */
+    double getX();
+
+    /**
+     * @brief Getter for the y coordinate of the point
+     * 
+     * @return The y coordinate of the point
+     */
+    double getY();
+
+    /**
+     * @brief Overloaded multiplication operator
+     * 
+     * @param scaleFactor The factor by which to scale the point
+     * @return The scaled point
+     */
+    Point operator*(double scaleFactor);
+
+    /**
+     * @brief Friend insertion operator to enable easy printing of Point objects
+     * 
+     * @param output The output to write to
+     * @param pt The Point object to be printed
+     * @return The output stream
+     */
+    friend std::ostream& operator<<(std::ostream& output, Point pt);
+
+    /**
+     * @brief Friend operator to perform scaling of a Point object
+     * 
+     * @param pt The Point object to be scaled
+     * @param tr The Transform object representing the scaling transformation
+     * @return The scaled Point object
+     */
+    friend Point& operator*=(Point& pt, Transform tr);
+};
+
+#endif // include guard for POINT_H
